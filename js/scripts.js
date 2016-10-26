5d2ad1fe9db0987d85032e570e2b87dc50e2818b
Andrea Faciane and Ellie Angell <ellie@>
2016-10-26 09:56:52 -0700

    return true for years divisble by 400

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,10 @@
 //business logic
 
 var leapYear = function(year) {
-  if (year % 4 === 0) {
+  if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
     return true;
   } else {
-  return false;
+    return false;
   }
 };
 
